7664bd969412f1a808475c0f29a70fc94559ba5b
akzo <anarkazimov98@hotmail.com>
2019-11-16 09:33:09 -0800

    uploading the foundation for genesis theme

---
+++ genesis-starter-theme/index.php
@@ -0,0 +1,11 @@
+<!-- Was inspired by https://youtu.be/FVqzKAUsM68?t=4040-->
+<h1><?php bloginfo('name') ?></h1>
+
+<?php
+  
+ function greet($name){
+    echo "<p> Hello we are $name</p>";
+ } 
+
+ greet("Website Paradise");
+?>
