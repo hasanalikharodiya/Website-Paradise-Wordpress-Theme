3d86fda002a88aa903f2a81625e948976d25db1b
akzo <anarkazimov98@hotmail.com>
2019-11-24 01:49:07 -0800

    fixed the footer issue

--- genesis-starter-theme/footer.php
+++ genesis-starter-theme/footer.php
@@ -1,4 +1,10 @@
-<h3 id="footer"> Theme By <a href ="https://websiteparadise.net/">Website Paradise</a> </h3>
+
+<div id="page-container">
+   <div id="content-wrap">
+   <h3 id="footer"> Theme By <a href ="https://websiteparadise.net/">Website Paradise</a> </h3>
+   </div>
+   <footer id="footer"></footer>
+ </div>
 
 <?php wp_footer();?>
 </body>
